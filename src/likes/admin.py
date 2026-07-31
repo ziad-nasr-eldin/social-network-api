6d01c9a78f6e6e84c081f0929baa9fa25047bc84
ziad <ziad11nasr11@gmail.com>
2026-08-01 00:01:31 +0300

    Configure like admin

--- src/likes/admin.py
+++ src/likes/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Like
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "post",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+    )
+    search_fields = (
+        "user__username",
+        "post__id",
+    )
+    autocomplete_fields = (
+        "user",
+        "post",
+    )
+    ordering = (
+        "-created_at",
+    )
