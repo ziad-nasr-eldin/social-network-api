936056097b0e52c099ac675f6d2cf4543b87b6c0
ziad nasr-eldin <ziad11nasr11@gmail.com>
2026-08-18 12:26:35 +0300

    Configure notification admin

--- src/notifications/admin.py
+++ src/notifications/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "recipient",
+        "actor",
+        "notification_type",
+        "is_read",
+        "created_at",
+    )
+    list_filter = (
+        "notification_type",
+        "is_read",
+        "created_at",
+    )
+    search_fields = (
+        "recipient__username",
+        "actor__username",
+    )
+    autocomplete_fields = (
+        "recipient",
+        "actor",
+        "post",
+        "comment",
+    )
+    ordering = (
+        "-created_at",
+    )
